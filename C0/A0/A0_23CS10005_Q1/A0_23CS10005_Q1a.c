1322c2a28fec16088bceac1d3a93693070e7051a
ashuwhy <ashutoshsharmawhy@gmail.com>
2025-01-03 18:30:23 +0530

    class 0 > lab 0 > assingment 0 > q1

---
+++ C0/A0/A0_23CS10005_Q1/A0_23CS10005_Q1a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Recursive function to find the length of a string
+int string_length(char *str) {
+    if (*str == '\0') {
+        return 0;
+    }
+    return 1 + string_length(str + 1);
+}
+
+// Main function to test the string length function
+int main() {
+    char str1[100];
+    printf("Enter a string: ");
+    scanf("%s", str1);
+    printf("The length of the string is: %d\n", string_length(str1));
+
+    return 0;
+}
---
+++ C0/A0/A0_23CS10005_Q1/A0_23CS10005_Q1b.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// Recursive function to find the sum of two strings
+long int sumof_two_strings(char *str1, char *str2) {
+    int len1 = 0, len2 = 0;
+    while (str1[len1] != '\0') len1++;
+    while (str2[len2] != '\0') len2++;
+    
+    len1--;
+    len2--;
+    
+    long int sum = 0;
+    long int place = 1;
+    
+    while (len1 >= 0 || len2 >= 0) {
+        if (len1 >= 0) {
+            sum += (str1[len1] - '0') * place;
+            len1--;
+        }
+        if (len2 >= 0) {
+            sum += (str2[len2] - '0') * place;
+            len2--;
+        }
+        place *= 10;
+    }
+    
+    return sum;
+}
+
+// Main function to test the sum of two strings function
+int main() {
+    char str2[100], str3[100];
+    printf("Enter string1 (number): ");
+    scanf("%s", str2);
+    printf("Enter string2 (number): ");
+    scanf("%s", str3);
+
+    printf("The sum of two strings is: %ld\n", sumof_two_strings(str2, str3));
+
+    return 0;
+}
